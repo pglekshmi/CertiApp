1541564079e616e148f2bf791ab42eee0f942214
lekshmi <pglekshmi88@gmail.com>
2023-10-19 10:22:13 +0530

    updated Certificate

--- ui/src/Components/Certificate.js
+++ ui/src/Components/Certificate.js
@@ -1,13 +1,14 @@
 import { useLocation } from "react-router-dom";
-export default function Certificate(props){
-    const {state}= useLocation();
-    const data = props.location.state;
+export default function Certificate(){
+    const location = useLocation();
+    const {state} = location
+    const data = state.data;
     console.log(data);
     return(
         <div>
-        <p className="text-right italic">cert-id</p><br></br><br></br>
+        <p className="text-right italic">{data.CertID}</p><br></br><br></br>
         <p className="text-center text-4xl font-mono">Kerala Blockchain Academy</p><br></br><br></br><br></br>
-        <p className="text-center font-sans ">This is to certify that name has successfully completed course with grade on date</p>
+        <p className="text-center font-sans ">This is to certify that {data.CandName} has successfully completed {data.CourseName} with {data.Grade} on {data.Date}</p>
         </div>
     )
 }
