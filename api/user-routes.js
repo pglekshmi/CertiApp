f86fc18be251492564d97ab6e6790dca163c1923
lekshmi <pglekshmi88@gmail.com>
2024-07-25 10:46:38 +0530

    updated

---
+++ api/user-routes.js
@@ -0,0 +1,64 @@
+import { Router } from 'express';
+// import CertiDetails from './Models/CertiDetails.js';
+var router = Router();
+const certDetails = new Map();
+router.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+router.post('/issue', async (req, res) => {
+    try {
+
+        const { certificateID, courseName, candidateName, grade, date } = req.body;
+        console.log(certificateID);
+
+        certDetails.set(certificateID, { courseName, candidateName, grade, date });
+        console.log(certDetails.get(certificateID));
+        // const result = await CertiDetails.create(data);
+        res.status(201).json({ message: 'saved' })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json();
+    }
+})
+
+router.get('/view/:id', async (req, res) => {
+    const id1 = req.params.id;
+    console.log(id1);
+    const details = certDetails.get(id1);
+    console.log(details);
+    // console.log(id);
+    // const details = await CertiDetails.findOne({CertID: id});
+    // console.log(details);
+    if (details) {
+        // res.cookie('certDetails',JSON.stringify(details));
+        res.json(details);
+    }
+    else {
+        res.status(404).json({ message: 'details not found' });
+    }
+
+})
+router.post('/update', (req, res) => {
+    try {
+        const { certificateID, courseName, candidateName, grade, date } = req.body;
+        console.log(certificateID);
+
+        certDetails.set(certificateID, { courseName, candidateName, grade, date });
+        // console.log(certDetails.get(certificateID));
+        // const result = await CertiDetails.create(data);
+        res.status(201).json({ message: 'saved' })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json();
+    }
+})
+
+router.get('/delete/:id', (req, res) => {
+    const id1 = req.params.id;
+    console.log(id1);
+    certDetails.delete(id1);
+    res.status(205).json({ msg: 'delete' })
+    // res.send({msg:'delete'});
+})
+export default router;
